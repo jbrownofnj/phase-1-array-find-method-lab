6ee5a0de92f22d1cdc041f30026fa5f8d1966e70
John Brown <johnbrownsinbox@gmail.com>
2022-02-27 20:23:32 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const aOO=[{year:2000,result:"W"},{year:2000,result:"d"},{year:2000,result:"d"}]
+
+function isItAWinner(object){
+    if(object.result==="W"){
+        return true;
+    }  
+    else {
+        return false;
+    }
+}
+
+function superbowlWin(arrayOfObjects){
+    const answer = arrayOfObjects.find(isItAWinner)
+    if (typeof(answer)==="object"){
+        return answer.year;
+    }
+    else{
+        return answer;
+    }
+}
+
+
+
